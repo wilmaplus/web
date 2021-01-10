c749935f8a92fca1fb4a3c5ff03613e202a22c3f
developerfromjokela <developerfromjokela@gmail.com>
2021-01-10 13:04:40 +0200

    Started creating database structure

---
+++ src/app/authapi/accounts_db/model.ts
@@ -0,0 +1,22 @@
+interface AccountModel {
+  id: string,
+  wilmaServer: string
+  username: string
+  password: string,
+  name: string,
+  type: number,
+  primusId: number,
+  formKey: string,
+  cookies: string,
+  settings: object,
+  photo: object,
+  school: string,
+  selectedRole: string,
+  fullscreenHomepage: boolean,
+  messageReceivedColor: string,
+  messageSentColor: string,
+  sendFabButton: string,
+  composeCardColor: string,
+  messageBackground: string,
+  darkTheme: boolean
+}
